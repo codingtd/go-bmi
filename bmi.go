e9c8cbca0867f8c48dc921cea72e61ae0cf63138
codingtd <codingtd@163.com>
2023-01-01 16:24:33 +0800

    Update bmi.go

--- bmi.go
+++ bmi.go
@@ -18,3 +18,15 @@ func BMI(weightKG, heightM float64) (bmi float64, err error) {
 	bmi = weightKG / (heightM * heightM)
 	return
 }
+
+
+func CalcFatRate(bmi float64, age int, sex string) (fatRate float64) {
+	sexWeight := 0
+	if sex == "男" {
+		sexWeight = 1
+	} else {
+		sexWeight = 0
+	}
+	fatRate = (1.2*bmi + getAgeWeight(age)*float64(age) - 5.4 - 10.8*float64(sexWeight)) / 100
+	return
+}
